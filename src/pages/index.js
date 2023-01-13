dd0f9cd9a3b6cf7d297be116fcaa9f75acb324bc
Alex Reed <alex.reed@gatsbyjs.com>
2023-01-13 11:36:40 -0500

    memo the block components

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,30 @@ import * as sections from "../components/sections"
 import Fallback from "../components/fallback"
 import SEOHead from "../components/head"
 
+const Block = React.memo(({ block }) => {
+  const { id, blocktype, ...componentProps } = block
+  const Component = sections[blocktype] || Fallback
+  console.log("rendering block: ", blocktype)
+  return (
+    <React.Suspense fallback={`Loading... ${blocktype}`}>
+      <Component key={id} {...componentProps} />
+    </React.Suspense>
+  )
+})
+
 export default function Homepage(props) {
+  const [fakeValue, setFakeValue] = React.useState(false)
+  React.useEffect(() => {
+    console.log("hey, there ", fakeValue)
+    setFakeValue(true)
+  }, [])
   const { homepage } = props.data
 
   return (
     <Layout>
-      {homepage.blocks.map((block) => {
-        const { id, blocktype, ...componentProps } = block
-        const Component = sections[blocktype] || Fallback
-        return <Component key={id} {...componentProps} />
-      })}
+      {homepage.blocks.map((block) => (
+        <Block block={block} />
+      ))}
     </Layout>
   )
 }
